78107b31bacb1b9acd51f2ca0d9405b80e8c1719
Segundo Fariña <segundofarina@MacBook-Pro-de-Segundo.local>
2019-03-13 20:26:57 -0300

    Fixed bug where FileChooser opened twice

--- src/main/java/ar/edu/itba/ati/Interface/Controller.java
+++ src/main/java/ar/edu/itba/ati/Interface/Controller.java
@@ -1,6 +1,16 @@
 package ar.edu.itba.ati.Interface;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public interface Controller {
 
+    int getPixelValue(int x ,int y);
+
+    void editPixelValue(int x, int y, int newValue);
+
+     BufferedImage loadImage(File image);
+
+     void saveImage(File image);
 
 }
