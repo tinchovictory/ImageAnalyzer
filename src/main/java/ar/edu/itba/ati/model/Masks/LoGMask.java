848f7d6b35ef5b2cb72e3b6d15129a92b3b1125c
tinchovictory <martin@victory.com.ar>
2019-04-22 15:25:55 -0300

    fix LoG mask

--- src/main/java/ar/edu/itba/ati/model/Masks/LoGMask.java
+++ src/main/java/ar/edu/itba/ati/model/Masks/LoGMask.java
@@ -6,7 +6,7 @@ public class LoGMask extends SimpleMask {
     private double deviation;
 
     public LoGMask(double deviation) {
-        super(3);
+        super(7);
         this.deviation = deviation;
     }
 
@@ -14,9 +14,11 @@ public class LoGMask extends SimpleMask {
     public double[][] getPoundedMask() {
         double[][] mask = new double[getMaskSize()][getMaskSize()];
 
-        for(int x = 0; x < mask.length; x++) {
-            for(int y = 0; y < mask[0].length; y++) {
-                mask[x][y] = Utils.getLaplaceGaussValue(x, y, deviation);
+        int borderLength = mask.length / 2;
+
+        for(int x = -borderLength, i = 0; x <= borderLength; x++, i++) {
+            for(int y = -borderLength, j = 0; y <= borderLength; y++, j++) {
+                mask[i][j] = Utils.getLaplaceGaussValue(x, y, deviation);
             }
         }
 
